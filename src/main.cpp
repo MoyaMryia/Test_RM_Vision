a16b7d8f09036f8d6026fa2175f4bf86f3c87f9f
MoyaMryia <MryiaMoya@outlook.com>
2025-09-19 11:34:59 +0800

    find some ways

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@
 #include "other_modules.hpp"
 #include <opencv2/opencv.hpp>
 
-//anyway give me a event everyday
-//
+//Step1 把assets/下面那个视频拉出来 使用opencv处理成图像然后挂事件
+//Step2 处理YOLO 
+//Step3 OPENCV confirm 装甲
+//Step4 预测
 
 int main(){
     

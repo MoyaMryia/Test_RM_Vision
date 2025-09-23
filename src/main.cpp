2ea81679e550d444e096c86cb64135acd7941a7b
MoyaMryia <MryiaMoya@outlook.com>
2025-09-23 20:56:27 +0800

    remove

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,11 @@
 //Step1 把assets/下面那个视频拉出来 使用opencv处理成图像然后挂事件
 //done
 //Step2 处理YOLO 
+//stucked, FUCK.
+//anyway I need a small function which can print square.
+//anyway.
+//anyway.
+
 //Step3 OPENCV confirm 装甲
 //Step4 预测
 int main(int argc, char** argv) {
@@ -14,32 +19,22 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    // 1. 初始化 VideoReader
     VideoReader reader(argv[1]);
 
     if (!reader.isOpened()) {
         std::cerr << "Error: Could not open the video file." << std::endl;
         return -1;
     }
-
     cv::Mat frame;
     cv::Mat processedFrame;
-
-    // 2. 主循环：读取、处理和显示
     while (true) {
         if (!reader.readFrame(frame)) {
             std::cout << "End of video stream or error occurred." << std::endl;
             break;
         }
-
-        // 处理帧
         processedFrame = VideoProcessor::processFrame(frame);
-
-        // 显示原始帧和处理后的帧
         cv::imshow("Original Video", frame);
         cv::imshow("Processed Video", processedFrame);
-
-        // 按 'q' 或 ESC 键退出
         if (cv::waitKey(25) == 'q' || cv::waitKey(25) == 27) {
             break;
         }

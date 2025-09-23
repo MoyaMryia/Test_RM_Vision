30f5b1bf770b1f2e636753125595a59cdfde79a5
MoyaMryia <MryiaMoya@outlook.com>
2025-09-23 21:01:15 +0800

    N/A

--- src/video_processor.cpp
+++ src/video_processor.cpp
@@ -1,13 +1,17 @@
 #include "../include/video_processor.hpp"
 
+void DrawRotatedRect(cv::Mat &image, const cv::RotatedRect &rotatedRect, const cv::Scalar &color, int thickness) {
+    cv::Point2f vertices[4];
+    rotatedRect.points(vertices); // 获取四个顶点
+// 绘制轮廓
+    for (int i = 0; i < 4; i++) {
+        line(image, vertices[i], vertices[(i + 1) % 4], color, thickness);
+    }
+}
+
 cv::Mat VideoProcessor::processFrame(const cv::Mat& inputFrame) {
-/*
-    cv::Mat grayFrame;
-    cv::cvtColor(inputFrame, grayFrame, cv::COLOR_BGR2GRAY);
-    */
     cv::Mat gray_img;
     cv::cvtColor(inputFrame,gray_img, cv::COLOR_BGR2GRAY);
-    //cv::GaussianBlur(grayFrame, processedFrame, cv::Size(5, 5), 0);
     cv::Mat binary_image;
     cv::threshold(gray_img,binary_image,120,255,cv::THRESH_BINARY);
     std::vector<std::vector<cv::Point>> contours;
@@ -18,6 +22,9 @@ cv::Mat VideoProcessor::processFrame(const cv::Mat& inputFrame) {
         rotaterects.emplace_back(rotaterect);
     }  
     cv::Mat processedFrame=inputFrame.clone();
-    cv::drawContours(processedFrame, contours, -1 ,cv::Scalar(0,0,255),2);
+    //cv::drawContours(processedFrame, contours, -1 ,cv::Scalar(0,0,255),2);
+    for(const auto & rectan : rotaterects){
+        DrawRotatedRect(processedFrame,rectan,cv::Scalar(0,255,0),2);
+    }
     return processedFrame;
 }
